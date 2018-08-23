8964583bab4657bfa13479f72dd10e05f67692dd
Lucas Veiga Fujicava <lucasveigaf@gmail.com>
2018-08-23 20:34:54 -0300

    Updated the logic for code testing and code submission

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,28 +2,76 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "../Navbar/Navbar";
 import CodeChallenge from "../CodeChallenge/CodeChallenge";
+import CodeOutput from "../CodeOutput/CodeOutput";
+import ChallengeIntro from "../ChallengeIntro/ChallengeIntro";
 import challenge from "../../mocks/challenge.json";
 
 class App extends Component {
+  state = {
+    codeOutput: "",
+    isWaitingResponse: false
+  };
+
+  onCodeTest = value => {
+    this.setState({ isWaitingResponse: true, error: false });
+  };
+
+  onResponse = response => {
+    this.setState({
+      codeOutput: response,
+      isWaitingResponse: false
+    });
+  };
+
+  onError = error => {
+    this.setState({
+      isWaitingResponse: false,
+      codeOutput: "Houve um erro. Por favor tente novamente.",
+      error: true
+    });
+  };
+
+  onCodeSubmit = value => {
+    // aqui enviariamos o código final do usuário ao back end.
+    alert("Teste enviado!");
+  };
+
   render() {
     return (
       <React.Fragment>
         <Navbar />
         <div className="container-fluid">
           <div className="row fill">
-            <div className="col-sm-4 col-md-4 col-lg-3 row m-0 p-0 challengeDescriptionWrapper">
-              <p>{challenge.description}</p>
+            <div className="col-sm-4 col-md-4 col-lg-3 m-0 p-0 p-2 challengeDescriptionWrapper">
+              <ChallengeIntro
+                title={challenge.title}
+                timeLimit={challenge.timeLimit}
+                description={challenge.description}
+              />
             </div>
-            <div className="col-sm-8 col-md-8 col-lg-9 row m-0 p-0 challengeMainWrapper">
+            <div className="col-sm-8 col-md-8 col-lg-9 row m-0 pl-6 challengeMainWrapper">
               <div className="col-sm-12 p-0">
                 <CodeChallenge
                   id="codeEditor"
-                  onChange={value => console.log(value)}
                   languages={challenge.languages}
+                  onCodeTest={this.onCodeTest}
+                  onResponse={this.onResponse}
+                  onError={this.onError}
+                  onSubmit={this.onCodeSubmit}
+                  timeLimit={challenge.timeLimit}
                 />
               </div>
               <div className="col-sm-12 challengeOutputWrapper">
-                <p>></p>
+                <CodeOutput
+                  isLoading={this.state.isWaitingResponse}
+                  value={this.state.codeOutput}
+                  startingText="> "
+                  spinnerHeight="100px"
+                  spinnerWidth="100px"
+                  spinnerColor="#A0A0A0"
+                  spinnerDelay={0}
+                  textColor={this.state.error ? "#af2446" : "#6b757d"}
+                />
               </div>
             </div>
           </div>
--- src/components/CodeChallenge/CodeChallenge.js
+++ src/components/CodeChallenge/CodeChallenge.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import CodeEditor from "../CodeEditor/CodeEditor";
 import "./CodeChallenge.css";
 import Select from "react-select";
@@ -6,8 +6,18 @@ import Select from "react-select";
 class CodeChallenge extends React.Component {
   state = {
     selectedOption: this.props.languages[0],
-    editorValue: ""
+    editorValue: "",
+    codeSubmitted: false
   };
+
+  componentDidMount() {
+    const { timeLimit } = this.props;
+    if (timeLimit) {
+      setTimeout(() => {
+        this.submitCode();
+      }, timeLimit);
+    }
+  }
 
   handleOptionChange = selectedOption => {
     this.setState({ selectedOption });
@@ -17,14 +27,70 @@ class CodeChallenge extends React.Component {
     this.setState({ editorValue });
   };
 
+  testCode = async () => {
+    try {
+      if (this.props.onCodeTest) {
+        this.props.onCodeTest(this.state.editorValue);
+      }
+      const rawResponse = await fetch(
+        "https://cors-anywhere.herokuapp.com/https://api.jdoodle.com/v1/execute",
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            script: this.state.editorValue,
+            language: this.state.selectedOption.value,
+            versionIndex: this.state.selectedOption.jdoodleVersion,
+            clientId: "dec68d0eaa2878f8e4b84c132d74a043",
+            clientSecret:
+              "b90b56835e0a22b9fd20dd46103260d345abe5971ef6c76d514a0ea37e945299"
+          })
+        }
+      );
+      let content = await rawResponse.json();
+      if (content.error) {
+        throw new Error(content.error);
+      }
+      if (this.props.onResponse) {
+        this.props.onResponse(content.output);
+      }
+    } catch (e) {
+      if (this.props.onError) {
+        this.props.onError(e);
+      }
+    }
+  };
+
+  submitCode = () => {
+    this.setState({ codeSubmitted: true });
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.state.editorValue);
+    }
+  };
+
   render() {
     return (
       <div className="codeChallenge">
         <div className="row codeChallengeTopRow">
           <div className="col-6 col-sm-7 col-md-7 col-lg-9">
             <div className="codeChallengeButtons">
-              <button className="btn btn-light">Testar</button>
-              <button className="btn btn-dark ml-2">Enviar</button>
+              <button
+                className="btn btn-light"
+                disabled={this.state.codeSubmitted}
+                onClick={this.testCode}
+              >
+                Testar
+              </button>
+              <button
+                className="btn btn-dark ml-2"
+                disabled={this.state.codeSubmitted}
+                onClick={this.submitCode}
+              >
+                Enviar
+              </button>
             </div>
           </div>
           <div className="col-6 col-sm-5 col-md-5 col-lg-3 codeEditorOptions">
