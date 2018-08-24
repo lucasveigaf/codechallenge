dc00c4ecf9a2a2848d8660dc4354b816e0860993
Lucas Veiga Fujicava <lucasveigaf@gmail.com>
2018-08-23 22:31:37 -0300

    Added CSS map and fixed small styling bug

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,8 +6,8 @@ import logo from "./img/logo.svg";
  */
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" target="_self" href="/">
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" target="_self" href="/">
         <img alt="logo" src={logo} />
       </a>
     </nav>
